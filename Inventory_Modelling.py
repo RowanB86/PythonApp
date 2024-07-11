d4e787b958f66c7165d8738d747607b0c44054ef
RowanB86 <rowan.barua@hotmail.co.uk>
2024-07-11 23:05:11 +0100

    Update Inventory_Modelling.py

--- Inventory_Modelling.py
+++ Inventory_Modelling.py
@@ -1,181 +1,254 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 09:13:27 2023
+
+@author: RowanBarua
+"""
+
 import streamlit as st
 import numpy as np
 from scipy import stats
+from scipy.stats import poisson
 import matplotlib.pyplot as plt
+import pandas as pd
 from matplotlib import rcParams
-import time
-
+import random
 rcParams.update({'figure.autolayout': True})
 
 class KDEDist(stats.rv_continuous):
+    
     def __init__(self, kde, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._kde = kde
-
+    
     def _pdf(self, x):
         return self._kde.pdf(x)
 
-def profile(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        st.write(f"Execution Time for {func.__name__}: {end_time - start_time} seconds")
-        return result
-    return wrapper
+st.title('Max Stock Holding / Re-Order Point Estimation')
 
-@profile
-def generate_lt_data():
+Chart_Types = ['PDF','CDF']
+Distributions = ['Poisson','Normal']
+
+st.header('Lead Time Modelling')
+LTDist = st.selectbox('Select Distribution',Distributions,key="LTDist")
+
+if LTDist == 'Normal':
     try:
-        LTMean = 10  # Hardcoded value for testing
-        LTStDev = 2  # Hardcoded value for testing
+        LTMean = st.text_input("Enter Lead Time Mean:",key="LTMean")
+        LTStDev = st.text_input("Enter Lead Time Standard Deviation:",key="LTStDev")
         AvgLT = float(LTMean)
         LTStDev2 = float(LTStDev)
         data = np.random.normal(AvgLT, LTStDev2, 1000)
         st.session_state.AvgLT = AvgLT
         st.session_state.LTStDev2 = LTStDev2
-        return data
-    except Exception as e:
-        st.write(f"Error generating lead time data: {e}")
-        return None
-
-@profile
-def generate_usage_data():
+    except:
+        pass
+else:
     try:
-        UsageMean = 20  # Hardcoded value for testing
-        UsageStDev = 5  # Hardcoded value for testing
+        LTMean = st.text_input("Enter Lead Time Mean:",key="LTMean")
+        AvgLT = float(LTMean)
+        data = np.random.poisson(AvgLT,  1000)
+        st.session_state.x = np.arange(0,AvgLT + ((AvgLT**0.5)*3))
+        st.session_state.pmf = poisson.pmf(st.session_state.x, AvgLT)
+        st.session_state.AvgLT = AvgLT
+    except:
+        pass
+
+LTChart = st.selectbox('Select Chart Type',Chart_Types,key="LTChart")
+LTChartButton = st.button("Generate Chart",key="LTChartButton")
+
+if LTChartButton:
+    if LTDist == 'Normal':
+        st.session_state.LTData = data
+        kde = stats.gaussian_kde(data)
+        X = KDEDist(kde)
+        inc = 1
+        x = np.arange(0, max(data), inc)
+        fig, axe = plt.subplots() 
+        fig.set_tight_layout(True)
+        #ax2 = axe.twinx() 
+        pdfVals = X.pdf(x)
+        cdfVals = X.cdf(x)
+        
+        
+        if LTChart == 'PDF':
+            axe.plot(x, X.pdf(x),color='r',label='PDF')
+            plt.title("PDF of Lead Time")
+        else:
+            axe.plot(x, X.cdf(x),color='r',label='CDF')
+            plt.title("CDF of Lead Time")
+        
+        st.plotly_chart(fig)
+    else:
+        
+        st.session_state.LTData = data
+        x = st.session_state.x
+        pmf = st.session_state.pmf
+        AvgLT = st.session_state.AvgLT
+        fig, axe = plt.subplots() 
+        fig.set_tight_layout(True)
+        #ax2 = axe.twinx() 
+        
+        if LTChart == 'PDF':
+            axe.plot(x, pmf,'o-',color='r',label='PDF')
+
+            plt.title("Probability Mass Function of Lead Time")
+        else:
+            cdf_vals = poisson.cdf(x, AvgLT)
+            axe.plot(x, cdf_vals,'o-',color='r',label='CDF')
+            plt.title("Cumulative Distribution Function of Lead Time")
+            
+        plt.xlabel('Lead Time (Days)', fontsize=14)
+        plt.ylabel('Probability', fontsize=14)
+        
+        st.plotly_chart(fig)        
+
+
+st.header('Usage Modelling')
+UsageDist = st.selectbox('Select Distribution',Distributions,key="UsageDist")
+
+if UsageDist == 'Normal':
+    try:
+        UsageMean = st.text_input("Enter Usage Mean:",key="UsageMean")
+        UsageStDev = st.text_input("Enter Usage Standard Deviation:",key="USageStDev")
         AvgMean = float(UsageMean)
         UsageStDev2 = float(UsageStDev)
         data = np.random.normal(AvgMean, UsageStDev2, 1000)
         st.session_state.AvgMean = AvgMean
         st.session_state.UsageStDev2 = UsageStDev2
-        return data
-    except Exception as e:
-        st.write(f"Error generating usage data: {e}")
-        return None
-
-@profile
-def generate_ltd_chart():
+    except:
+        pass
+else:
     try:
-        LTData = np.array(st.session_state.LTData)
-        UsageData = np.array(st.session_state.UsageData)
-        st.write("Generating Lead Time Demand data...")
+        UsageMean = st.text_input("Enter Usage Mean:",key="UsageMean")
+        AvgUsage = float(UsageMean)
+        data = np.random.poisson(AvgUsage, 1000)
+        st.session_state.x = np.arange(0,AvgUsage + ((AvgUsage**0.5)*3))
+        st.session_state.pmf = poisson.pmf(st.session_state.x, AvgUsage)
+        st.session_state.AvgUsage = AvgUsage
         
-        # Simplified data generation to identify the issue
-        generated_data = []
-        for LT in np.random.choice(LTData, size=1000, replace=True):
-            generated_data.append(np.sum(np.random.choice(UsageData, size=int(LT), replace=True)))
-        data = np.array(generated_data)
+    except:
+        pass
+
+UsageChart = st.selectbox('Select Chart Type',Chart_Types,key="UsageChart")
+UsageChartButton = st.button("Generate Chart",key="UsageChartButton")
+
+if UsageChartButton:
+    
+    if UsageDist == 'Normal':
+        st.session_state.UsageData = data
+        kde = stats.gaussian_kde(data)
+        X = KDEDist(kde)
+        inc = 1
+        x = np.arange(0, max(data), inc)
+        fig, axe = plt.subplots() 
+        fig.set_tight_layout(True)
+        #ax2 = axe.twinx() 
+        pdfVals = X.pdf(x)
+        cdfVals = X.cdf(x)
         
-        st.write(f"Generated data: {data[:10]}...")
-
-        @profile
-        def calculate_kde():
-            st.write("Calculating KDE...")
-            kde = stats.gaussian_kde(data)
-            X = KDEDist(kde)
-            st.write("KDE calculated.")
-            return X
-
-        X = calculate_kde()
-
-        @profile
-        def generate_plot_data():
-            st.write("Generating plot data...")
-            inc = 1
-            x = np.arange(0, max(data) + 3 * np.std(data), inc)
-            pdfVals = X.pdf(x)
-            cdfVals = X.cdf(x)
-            st.write("Plot data generated.")
-            return x, pdfVals, cdfVals
         
-        x, pdfVals, cdfVals = generate_plot_data()
+        if UsageChart == 'PDF':
+            axe.plot(x, X.pdf(x),color='r',label='PDF')
+            plt.title("PDF of Usage")
+        else:
+            axe.plot(x, X.cdf(x),color='r',label='CDF')
+            plt.title("CDF of Usage")
+    else:
+        st.session_state.UsageData = data
+        x = st.session_state.x
+        pmf = st.session_state.pmf
+        AvgUsage = st.session_state.AvgUsage
+
+        fig, axe = plt.subplots() 
+        fig.set_tight_layout(True)
+        #ax2 = axe.twinx() 
         
-        Quantile = 1 - 0.05  # Hardcoded value for testing
+        if UsageChart == 'PDF':
+            axe.plot(x, pmf,'o-',color='r',label='PDF')
 
-        @profile
-        def calculate_reorder_point():
-            st.write("Calculating reorder point...")
-            st.write(f"x: {x}")
-            st.write(f"cdfVals: {cdfVals}")
+            plt.title("Probability Mass Function of Usage")
+        else:
+            cdf_vals = poisson.cdf(x,AvgUsage)
+            axe.plot(x, cdf_vals,'o-',color='r',label='CDF')
+            plt.title("Cumulative Distribution Function of Usage")
             
-            min_val = min(i for i in cdfVals if i > Quantile)
-            st.write(f"min_val: {min_val}")
-            
-            min_val_ind = cdfVals.tolist().index(min_val)
-            st.write(f"min_val_ind: {min_val_ind}")
-            
-            UB1 = x[min_val_ind]
-            LB1 = x[min_val_ind - 1]
-            st.write(f"UB1: {UB1}, LB1: {LB1}")
-            
-            Rng1 = UB1 - LB1
-            st.write(f"Rng1: {Rng1}")
-            
-            UB2 = cdfVals[min_val_ind]
-            LB2 = cdfVals[min_val_ind - 1]
-            st.write(f"UB2: {UB2}, LB2: {LB2}")
-
-            Rng2 = UB2 - LB2
-            st.write(f"Rng2: {Rng2}")
-            
-            Factor = (Quantile - LB2) / Rng2
-            st.write(f"Factor: {Factor}")
-            
-            ROP = LB1 + (Factor * Rng1)
-            ROQ = (365 / 12) * st.session_state.AvgUsage
-            MSL = ROP + ROQ
-            st.write(f"ROP: {ROP}, ROQ: {ROQ}, MSL: {MSL}")
-            return ROP, MSL
+        plt.xlabel('Usage (Days)', fontsize=14)
+        plt.ylabel('Probability', fontsize=14)
         
-        ROP, MSL = calculate_reorder_point()
+        st.plotly_chart(fig)    
 
-        @profile
-        def generate_plots():
-            st.write("Generating plots...")
-            fig, axe = plt.subplots(figsize=(10, 6)) 
-            fig.set_tight_layout(True)
-            ax2 = axe.twinx()
-
-            if LTDChart == 'PDF':
-                axe.plot(x, pdfVals, color='r', label='PDF')
-                plt.title("PDF of Lead Time Demand")
-            else:
-                axe.plot(x, cdfVals, color='r', label='CDF')
-                plt.title("CDF of Lead Time Demand")
-
-            ax2.axvline(x=ROP, color='m', label='Re-Order Point')
-            ax2.axvline(x=MSL, color='y', label='Max Stock Level')
-            plt.draw()
-            ax2.legend(loc='upper left')
-            
-            st.pyplot(fig)
-            st.write("Plots generated.")
-
-        generate_plots()
-        
-        st.write('Re-Order Point: ' + str(round(ROP)))
-        st.write('Max Stock Level: ' + str(round(MSL)))
-    except Exception as e:
-        st.write(f"Error generating Lead Time Demand chart: {e}")
-
-# Main Code Execution
-st.title('Max Stock Holding / Re-Order Point Estimation')
-
-# Generate Lead Time Data
-st.header('Lead Time Modelling')
-lt_data = generate_lt_data()
-st.session_state.LTData = lt_data if lt_data is not None else []
-
-# Generate Usage Data
-st.header('Usage Modelling')
-usage_data = generate_usage_data()
-st.session_state.UsageData = usage_data if usage_data is not None else []
-
-# Generate Lead Time Demand Chart
 st.header('Lead Time Demand Modelling')
-LTDChart = st.selectbox('Select Chart Type', ['PDF', 'CDF'], key="LTDChart")
-LTDChartButton = st.button("Generate Chart", key="LTDChartButton")
+
+try:
+    ROF = int(st.text_input("Enter Re-Order Frequency (orders / year):",key="ROF"))
+except:
+    pass
+
+try:
+    ProbStockout = float(st.text_input("Enter Probability of Stockout:",key="StockoutProb"))
+except:
+    pass
+
+LTDChart = st.selectbox('Select Chart Type',Chart_Types,key="LTDChart")
+LTDChartButton = st.button("Generate Chart",key="LTDChartButton")
 
 if LTDChartButton:
-    generate_ltd_chart()
+    #data = np.multiply(st.session_state.LTData,st.session_state.UsageData)
+    
+    data = [None] * 1000
+    
+    for i in range(0,1000):
+        LT = random.sample(list(st.session_state.LTData),1)
+        data[i] = np.sum(random.sample(list(st.session_state.UsageData),LT[0]))
+    
+    kde = stats.gaussian_kde(data)
+    X = KDEDist(kde)
+    inc = 1
+    x = np.arange(0, max(data)+ 3*np.std(data), inc)
+    fig, axe = plt.subplots(figsize=(10, 6)) 
+    fig.set_tight_layout(True)
+    ax2 = axe.twinx() 
+    pdfVals = X.pdf(x)
+    cdfVals = X.cdf(x)    
+    Quantile = 1 - ProbStockout
+    
+    min_val = min(i for i in cdfVals if i > (1-ProbStockout))   
+    min_val_ind = cdfVals.tolist().index(min_val)
+    
+    UB1 = x[min_val_ind]
+    LB1 = x[min_val_ind-1]
+    
+    Rng1 = UB1 - LB1
+    
+    UB2 = cdfVals[min_val_ind]
+    LB2 = cdfVals[min_val_ind-1]
+
+    Rng2 = UB2 - LB2
+    Factor = (Quantile-LB2) / Rng2
+    ROP = LB1 + (Factor*Rng1)
+    ROQ = (365 / ROF) * st.session_state.AvgUsage
+    MSL = ROP + ROQ
+
+    if LTDChart == 'PDF':
+        axe.plot(x, pdfVals,color='r',label='PDF')
+        plt.title("PDF of Lead Time Demand")
+    else:
+        axe.plot(x, cdfVals,color='r',label='CDF')
+        plt.title("CDF of Lead Time Demand")
+    
+    ax2.axvline(x=ROP,color='m',label='Re-Order Point')
+    ax2.axvline(x=MSL,color='y',label='Max Stock Level')
+    # Adjust the subplot parameters to make room for the legend
+    #plt.subplots_adjust(right=0.3)  # Adjust this value as needed to fit your legend
+    plt.draw()
+    # Add the legend outside the plot
+    ax2.legend(loc='upper left')
+    
+    # Apply tight layout with a pad to fit the legend
+    plt.savefig('adjusted_plot.png')
+
+    st.pyplot(fig)
+    
+    st.write('Re-Order Point: ' + str(round(ROP)))
+    st.write('Max Stock Level: ' + str(round(MSL)))
