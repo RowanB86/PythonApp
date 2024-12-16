a3dd6399dd0e1174866e47de8987a7060f8425b2
RowanB86 <rowan.barua@hotmail.co.uk>
2024-12-16 22:25:15 +0000

    Update murder_mystery.py

--- murder_mystery.py
+++ murder_mystery.py
@@ -96,18 +96,15 @@ if not firebase_admin._apps:
         'databaseURL': 'https://murder-mystery-eb53d-default-rtdb.europe-west1.firebasedatabase.app'
     })
 
-def UpdatePlayerCharacterList(game,username):
+def UpdatePlayerCharacterList(game):
     player_character_list = ['Alfred Penrose','Captain Theodore Drake','Charlotte Fontain','Detective Hugh Barrington', \
                           'Dr. Horace Bellamy','Eleanor Winslow','Isabella Moretti','Lady Vivian Blackthorn', \
                           'Percy Hargrove','Reginald Reggie Crowley']   
-    ref = db.reference('players_in_game')
+    ref = db.reference('player_characters')
     players = ref.order_by_child("game").equal_to(st.session_state['game']).get() 
     if players:
-     
-
-        for player_id,player_data in players.items():
-            if player_data["player] = username:
-                player_character_list.pop(player_data["character"]
+        for player_id,player_data in players.
+            player_character_list.pop(player_data["character"]
 
     return(player_character_list)
 
