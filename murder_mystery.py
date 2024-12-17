83a5ebb72d5d01d1831bd3519b6bd73941b5873b
RowanB86 <rowan.barua@hotmail.co.uk>
2024-12-17 00:17:44 +0000

    Update murder_mystery.py

--- murder_mystery.py
+++ murder_mystery.py
@@ -333,10 +333,18 @@ if st.session_state['loggedIn']:
         player_characters = ref.get() 
         for player_id,player_data in player_characters.items():
           if player_data["username"] == st.session_state["username"]:
-              st.session_state["user_character"] = player_data["character"]
-            
-        ref = db.reference(f"player_characters/{st.session_state['player_id']}")
-        ref.delete()
+              ref = db.reference(f"player_characters/{st.session_state['player_id']}")
+              ref.delete()
+              break
+
+        ref = db.reference("players_in_game")
+        players = ref.get()
+        for player_id,player_data in players.items():
+            if player_data["player"] == st.session_state["username"]:
+              ref = db.reference(f"players_in_game/{st.session_state['player_id']}")
+              ref.delete()
+              break                
+        
         st.session_state['player_character_chosen'] = False
         st.session_state['player_in_game'] = False
         st.session_state["player_character_list"] = UpdatePlayerCharacterList(st.session_state["game_name"])
