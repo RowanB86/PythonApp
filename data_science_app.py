724cbb8d1d5565457cde9c369db22b616f97bf59
RowanB86 <rowan.barua@hotmail.co.uk>
2025-01-24 15:56:10 +0000

    Update data_science_app.py

--- data_science_app.py
+++ data_science_app.py
@@ -90,7 +90,7 @@ else:
         with st.expander("Datasets"):
             ref = db.reference("datasets")
             datasets = ref.get()
-            dataset_list = []
+            dataset_list = ["training_set"]
 
             if datasets is not None:
                 for dataset_id,dataset in datasets.items():
