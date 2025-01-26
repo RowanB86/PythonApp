84ca77b348c15e8f2a00f4b9fe41c98a68619ec1
RowanB86 <rowan.barua@hotmail.co.uk>
2025-01-26 22:01:06 +0000

    Update data_science_app.py

--- data_science_app.py
+++ data_science_app.py
@@ -184,8 +184,9 @@ else:
                     st.session_state['sql_code'] = updated_code
 
                 try:
-                    st_ace(value=st.session_state['sql_code'], language='sql', theme='monokai', key='ace-editor2')
+                    
                     code = DuckDBTransform(st.session_state['sql_code'])
+                    st_ace(value=code, language='sql', theme='monokai', key='ace-editor2')
                     local_namespace = {}
                     exec(code,{},local_namespace)
                     st.session_state["df_transform"] = local_namespace.get("df")
