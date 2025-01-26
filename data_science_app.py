548559469d2ff970a813a994630a710afc7dac63
RowanB86 <rowan.barua@hotmail.co.uk>
2025-01-26 12:55:21 +0000

    Update data_science_app.py

--- data_science_app.py
+++ data_science_app.py
@@ -174,11 +174,11 @@ else:
                 if updated_code != st.session_state['sql_code']:
                     st.session_state['sql_code'] = updated_code
                     
-                code = SQLTransform(st.session_state['sql_code'])
-                st_ace(value=code, language='sql', theme='monokai', key='ace-editor2')
+                #code = SQLTransform(st.session_state['sql_code'])
+                code = DuckDBTransform(st.session_state['sql_code'])
                 local_namespace = {}
                 exec(code,{},local_namespace)
                 df = local_namespace.get("df")
                 st.dataframe(df)
-                #st.rerun()
-                
+      
+              
